a80727a72f8f7c16f40496994e89f741430d1fc3
Kashif Rizvee <rizveekashif4u@gmail.com>
2020-11-02 06:11:49 +0000

    added eqArrays and its test file under project lotide

--- eqArrays.js
+++ eqArrays.js
@@ -1,35 +1,27 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
 
-  let res = true;
-
+  let result = true;
+  
   if (arr1.length !== arr2.length) {
-    // console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+    console.log('inside of IF BLOCK since both arrays are not equal in Length : ',arr1.length === arr2.length);
     return false;
   }
 
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      // console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+    
+    if (Array.isArray(arr1[i])) {
+      result = eqArrays(arr1[i], arr2[i]);
+      // if (result === false){
+      //   return false;
+      // }
+    } else if (arr1[i] !== arr2[i]) {
       return false;
     }
+    
   }
-  // console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  return res;
+  return result;
 
-}
+};
+module.exports = eqArrays;
 
 
-assertEqual(eqArrays([1,2,3],[1,2,3]), true);
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true)
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false)
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]) ,true)
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false)

3f3d502dbfe7632cd9a48cf1fa8318911f913778
Kashif Rizvee <rizveekashif4u@gmail.com>
2020-11-02 21:11:07 +0000

    added head - middle - tail functionality of Lotide project along with their test files written in mocha framework and chai assertion library

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,24 @@
-const assertEqualTail = require('../assertEqualTail');
-const tail = require('../tail');
+const assert = require("chai").assert;
+const tail = require("../tail");
 
-assertEqualTail(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
-assertEqualTail(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
-assertEqualTail(tail([1, 2, 3, 5]), [2, 3, 4, 5]);
-assertEqualTail(tail([1]), []);
-assertEqualTail(tail([]), []);
+describe("#tail", () => {
+
+  it('returns [2,3,4,5] for [1,2,3,4,5]', () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+  });
+
+  it('returns ["Lighthouse", "Labs"] for ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+  it('returns [] for [1]', () => {
+    assert.deepEqual(tail([1]), []);
+  });
+
+  it('returns [] for []', () => {
+    assert.deepEqual(tail([]), []);
+  });
+
+});
 
 

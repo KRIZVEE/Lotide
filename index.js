749718fc3d2a6f7843b1b4adbf1fdcd192168eb1
Kashif Rizvee <rizveekashif4u@gmail.com>
2020-11-02 21:47:13 +0000

    added index.js file exporting all the files related to lotide project

---
+++ index.js
@@ -0,0 +1,38 @@
+const head = require("./head");
+const middle = require("./middle");
+const tail = require("./tail");
+const without = require("./without");
+const takeUntil = require("./takeUntil");
+const map = require("./map");
+const letterPositions = require("./letterPositions");
+const flatten = require("./flatten");
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const eqObjects = require("./eqObjects");
+const eqArrays = require("./eqArrays");
+const countOnly = require("./countOnly");
+const countLetters = require("./countLetters");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const assertEqual = require("./assertEqual");
+const assertArraysEqual = require("./assertArraysEqual");
+
+
+module.exports = {
+  head: head,
+  middle: middle,
+  tail: tail,
+  without: without,
+  takeUntil: takeUntil,
+  map: map,
+  letterPositions: letterPositions,
+  flatten: flatten,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  eqObjects: eqObjects,
+  eqArrays: eqArrays,
+  countOnly: countOnly,
+  countLetters: countLetters,
+  assertObjectsEqual: assertObjectsEqual,
+  assertEqual: assertEqual,
+  assertArraysEqual: assertArraysEqual
+};

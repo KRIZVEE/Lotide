1bb42be49137cc7256b908d889b2520937f84eb8
Kashif Rizvee <rizveekashif4u@gmail.com>
2020-11-02 19:37:53 +0000

    added files related to lotide project

--- takeUntil.js
+++ takeUntil.js
@@ -1,26 +1,3 @@
-const assertEqual = function(actual, expected) {
-  eqArrays(actual, expected);
-};
-
-const eqArrays = function(arr1, arr2) {
-
-  let res = true;
-
-  if (arr1.length !== arr2.length) {
-    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
-      return false;
-    }
-  }
-  console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  return res;
-};
-
 const takeUntil = function(array, callback) {
   let results = [];
   for (let item of array) {
@@ -33,14 +10,4 @@ const takeUntil = function(array, callback) {
   return results;
 };
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-// const results1 = takeUntil(data1, x => x < 0);
-// console.log(results1);
-assertEqual(takeUntil(data1, x => x < 0), [ 1, 2, 5, 7, 2 ]);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-// const results2 = takeUntil(data2, x => x === ',');
-// console.log(results2);
-assertEqual(takeUntil(data2, x => x === ','), [ "I've", 'been', 'to', 'Hollywood' ]);
+module.exports = takeUntil;
--- without.js
+++ without.js
@@ -1,47 +1,12 @@
-const assertArraysEqual = function(arr1, arr2){
-  return eqArrays(arr1,arr2)
-}
-
-const eqArrays = function(arr1, arr2) {
-
-  let res = true;
-
-  if (arr1.length !== arr2.length) {
-    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
-      return false;
-    }
-  }
-  console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  return res;
-
-}
-
-const without = function (source, itemsToRemove) {
+const without = function(source, itemsToRemove) {
   let res = [];
-  let i=0; let j = 0;
 
   for (let i = 0; i < source.length; i++) {
-      if(!itemsToRemove.includes(source[i]))    {
-        res.push(source[i])
-      }
+    if (!itemsToRemove.includes(source[i]))    {
+      res.push(source[i]);
+    }
   }
-  return res
-}
+  return res;
+};
 
-
-// console.log(without([1, 2, 3,4,5,'bhb'], [1,2,3,4])) // [2,3]
-without([1,2,3], [1]) // [2,3]
-assertArraysEqual(without([1,2,3], [1]),[2,3])
-// console.log(without(["1","2","3"],[1,2,"3"])) // ["1","2"]
-
-const words = ["hello", "world", "lighthouse"];
-assertArraysEqual(without(words, ["lighthouse"]), ["hello", "world", "lighthouse"]);
-assertArraysEqual(without(words, ["lighthouse"]), ["hello", "world"]);
-assertArraysEqual(without([1, 2, 3], [1]), [2, 3])
-assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"])
+module.exports = without;
